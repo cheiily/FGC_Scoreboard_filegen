34814969e21807a2d58e1f47d113f2f8d4914c7a
_cheily <cheiily.pm@gmail.com>
2024-09-13 03:51:07 +0200

    haha oopsie

--- src/main/java/pl/cheily/filegen/LocalData/FileManagement/Meta/Players/PlayersDAO.java
+++ src/main/java/pl/cheily/filegen/LocalData/FileManagement/Meta/Players/PlayersDAO.java
@@ -6,9 +6,8 @@ import pl.cheily.filegen.LocalData.Player;
 import java.util.List;
 
 public interface PlayersDAO extends DAO<Player> {
-    List<Player> findByTag(String tag);
-    List<Player> findByNationality(String nationality);
-    List<Player> findByName(String name);
-    List<Player> findByRemoteId(long remoteId);
-    // setAll
+    public List<Player> findByTag(String tag);
+    public List<Player> findByNationality(String nationality);
+    public List<Player> findByName(String name);
+    public List<Player> findByRemoteId(long remoteId);
 }
